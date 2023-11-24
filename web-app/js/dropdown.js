07f132fa06f0574d6cd725a9c865c0437c2d90c0
Aaron Tarajos <aarontarajos@midpointmarkets.org>
2023-11-24 14:00:47 -0800

    dropdown adjustments and ranged update

--- web-app/js/dropdown.js
+++ web-app/js/dropdown.js
@@ -1,3 +1,5 @@
+const magicOpts = requre('./magic/utils/magic_const.js')
+
 function changeOption(optionText, dropdownId) {
   var dropbtn = document.getElementById(dropdownId);
   dropbtn.textContent = optionText;
@@ -9,3 +11,29 @@ document.querySelectorAll('.option').forEach(function(element) {
     event.preventDefault();
   });
 });
+
+
+// Example dictionary
+const myOptions = {
+  option1: "Value 1",
+  option2: "Value 2",
+  option3: "Value 3"
+  // Add more options as needed
+};
+
+// Function to populate the dropdown
+function populateDropdown() {
+  const dropdown = document.getElementById("myDropdown");
+
+  for (const key in myOptions) {
+      if (myOptions.hasOwnProperty(key)) {
+          const option = document.createElement("option");
+          option.value = key;
+          option.text = myOptions[key];
+          dropdown.appendChild(option);
+      }
+  }
+}
+
+// Call the function to populate the dropdown
+populateDropdown();
