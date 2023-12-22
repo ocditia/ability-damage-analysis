b72b980a189895520f43f45e34c03fb0797c6466
Aaron Tarajos <aarontarajos@midpointmarkets.org>
2023-12-22 11:18:34 -0800

    removing un-used scripts

--- necromancy/utils/necromancy_abils.js
+++
@@ -1,8 +0,0 @@
-const abils = {
-    "basic attack":
-    {
-        const min_percent = 0.9;
-        const var_percent = 0.2,
-        const numberOfHits = 1;
-    }
-}
---
+++ ranged/ranged_const.js
@@ -0,0 +1,379 @@
+const necromancy_construction = {
+    'two-handed weapons': {
+    },
+
+    'main-hand weapons': {
+        "blightbound crossbow":{
+            "tier": 92,
+            "weaponDmg": 0
+        },
+    },
+
+    'off-hand weapons': {
+        "blightbound crossbow":{
+            "tier": 92,
+            "weaponDmg": 0
+        },
+    },
+
+    'shields': {
+    },
+
+    'defenders': {
+    },
+
+    'helmets': {
+        "Elite Dracolich":{
+            "bonus": 24
+        },
+        "Elite Sirenic":{
+            "bonus": 24
+        },
+    },
+
+    'bodies': {
+        "Elite Dracolich":{
+            "bonus": 36
+        },
+        "Elite Sirenic":{
+            "bonus": 36
+        },
+    },
+
+    'legs': {
+        "Elite Dracolich":{
+            "bonus": 31
+        },
+        "Elite Sirenic":{
+            "bonus": 31
+        },
+    },
+
+    'boots': {
+        "Enhanced Fleeting":{
+            "bonus": 14.5
+        },
+        "Silverhawk boots (tier 60)":{
+            "bonus": 6.9
+        }
+    },
+
+    'gloves': {
+        "Enhanced Nightmare":{
+            "bonus": 14.5
+        },
+    },
+
+    'necklaces': {
+        "Essence of finality amulet (or)":{
+            "bonus": 59
+        },
+        "Essence of finality amulet":{
+            "bonus": 56
+        },
+        "Amulet of souls (or)":{
+            "bonus": 48
+        },
+        "Amulet of souls":{
+            "bonus": 46.3
+        },
+        "Reaper (or)":{
+            "bonus": 38.4
+        },
+        "Reaper necklace":{
+            "bonus": 36.8
+        },
+        "Salve (e)":{
+            "bonus": 4.2
+        },
+        "Salve":{
+            "bonus": 0
+        },
+        "Dragon rider necklace":{
+            "bonus": 30.2
+        }
+    },
+
+    'rings': {
+        "Reavers":{
+            "bonus": 27.7
+        },
+        "Stalkers":{
+            "bonus": 30.4
+        },
+        "Ring of death (i)":{
+            "bonus": 27.7
+        },
+        "Ring of death":{
+            "bonus": 25.2
+        },
+        "ASR (i)":{
+            "bonus": 23.4
+        },
+        "ASR":{
+            "bonus": 21
+        }
+    },
+
+    'capes': {
+        "Igneous kal-mor":{
+            "bonus": 43
+        },
+        "Completionist":{
+            "bonus": 31.5
+        },
+        "Tokhaar-kal-mor":{
+            "bonus": 33
+        },
+        "God cape":{
+            "bonus": 31.5
+        },
+        "Max cape":{
+            "bonus": 31.5
+        },
+        "Skillcape":{
+            "bonus": 21.6
+        }
+    },
+
+    'pockets': {
+        "Grimoire":{
+            "bonus": 7
+        },
+        "Scripture of jas":{
+            "bonus": 8
+        },
+        "Scripture of ful":{
+            "bonus": 8
+        },
+        "Scripture of wen":{
+            "bonus": 8
+        },
+        "Illuminated god book":{
+            "bonus": 7
+        },
+        "God book":{
+            "bonus": 5
+        }
+    },
+
+    'auras': {
+        "Mahjarrat":{
+            "levels": 0.0,
+            "boost": 0.05
+        },
+        "Reckless":{
+            "levels": 0.1,
+            "boost": 0.1
+        },
+        "Equilibrium":{
+            "levels": 0.0,
+            "boost": 0.0
+        }
+    },
+
+    'prayers': {
+        "desolation":{
+            "boost": 0.12
+        },
+        "anguish":{
+            "boost": 0.10
+        },
+        "rigour":{
+            "boost": 0.08
+        },
+        "accelerated decay":{
+            "boost": 0.06
+        },
+        "hastened decay":{
+            "boost": 0.06
+        },
+        "decay":{
+            "boost": 0.02
+        }
+    },
+
+    'potions': {
+        "elder overload":{
+            'levels': 5,
+            'percent': 0.17
+        },
+        "supreme overload":{
+            'levels': 4,
+            'percent': 0.16
+        },
+        "overload":{
+            "levels": 3,
+            "percent": 0.15
+        },
+        "extreme necromancy":
+        {
+            "levels": 3,
+            "percent": 0.15
+        },
+        "super necromancy":
+        {
+            "levels": 2,
+            "percent": 0.12
+        },
+        "necromancy potion":
+        {
+            "levels": 1,
+            "percent": 0.08
+        }
+    },
+
+    'slayerHelmets': {
+        "none":{
+            "boost": 0
+        },
+        "Slayer helmet":{
+            "boost": 0.075
+        },
+        "Full slayer helmet":{
+            "boost": 0.075
+        },
+        "Reinforced slayer helmet":{
+            "boost": 0.08
+        },
+        "Strong slayer helmet":{
+            "boost": 0.085
+        },
+        "Mighty slayer helmet":{
+            "boost": 0.09
+        },
+        "Corrupted slayer helmet":{
+            "boost": 0.095
+        }
+    },
+
+    'abilities': {
+        "basic attack":{
+            "fixed percent": 0.9,
+            "variable percent": 0.2,
+            "on hit effects": true,
+            "crit effects": true
+        },
+        "skeleton auto":{
+            "fixed percent": 0.22,
+            "variable percent": 0.06,
+            "on hit effects": false,
+            "crit effects": false
+        },
+        "finger of death":{
+            "fixed percent": 2.7,
+            "variable percent": 0.6,
+            "on hit effects": true,
+            "crit effects": true
+        },
+        "touch of death":{
+            "fixed percent": 0.9,
+            "variable percent": 0.2,
+            "on hit effects": true,
+            "crit effects": true
+        },
+        "death skulls":{
+            "fixed percent": 2.25,
+            "variable percent": 0.5,
+            "on hit effects": true,
+            "crit effects": true
+        },
+        "blood siphon - siphon":{
+            "fixed percent": 0.22,
+            "variable percent": 0.06
+        },
+        "blood siphon - hit":{
+            "fixed percent": 1.17,
+            "variable percent": 0.26,
+            "on hit effects": true,
+            "crit effects": true
+        },
+        "zombie auto":{
+            "fixed percent": 0.18,
+            "variable percent": 0.04,
+            "on hit effects": false,
+            "crit effects": false        
+        },
+        "zombie poison":{
+            "fixed percent": 0.08,
+            "variable percent": 0.04
+        },
+        "command zombie":{
+            "fixed percent": 0.08,
+            "variable percent": 0.04,
+            "on hit effects": false,
+            "crit effects": false
+        },
+        "ghost auto":{
+            "fixed percent": 0.18,
+            "variable percent": 0.04,
+            "on hit effects": false,
+            "crit effects": false
+        },
+        "bloat - initial hit":{
+            "fixed percent": 1.35,
+            "variable percent": 0.3,
+            "on hit effects": true,
+            "crit effects": true
+        },
+        "bloat - bleed hit":{
+            "number of hits": 10
+        },
+        "soul sap":{
+            "fixed percent": 0.9,
+            "variable percent": 0.2,
+            "on hit effects": true,
+            "crit effects": true
+        },
+        "soul strike - main":{
+            "fixed percent": 1.35,
+            "variable percent": 0.3,
+            "on hit effects": true,
+            "crit effects": true
+        },
+        "soul strike - aoe":{
+            "fixed percent": 0.9,
+            "variable percent": 0.2,
+            "on hit effects": true,
+            "crit effects": true
+        },
+        "spectral scythe - 1":{
+            "fixed percent": 0.72,
+            "variable percent": 0.16,
+            "on hit effects": true,
+            "crit effects": true
+        },
+        "spectral scythe - 2":{
+            "fixed percent": 1.8,
+            "variable percent": 0.4,
+            "on hit effects": true,
+            "crit effects": true
+        },
+        "spectral scythe - 3":{
+            "fixed percent": 2.25,
+            "variable percent": 0.5,
+            "on hit effects": true,
+            "crit effects": true
+        },
+        "volley of souls":{
+            "fixed percent": 1.35,
+            "variable percent": 0.3,
+            "on hit effects": true,
+            "crit effects": true
+        },
+        "death grasp":{
+            "fixed percent": 4.05,
+            "variable percent": 4.95,
+            "on hit effects": true,
+            "crit effects": true
+        },
+        "death essence":{
+            "fixed percent": 3.6,
+            "variable percent": 0.8,
+            "on hit effects": true,
+            "crit effects": true
+        }
+    }
+}
+
+module.exports = necromancy_construction;
