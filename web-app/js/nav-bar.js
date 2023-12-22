42b5c326823733bc808928796bb242b557dfa408
Aaron Tarajos <aarontarajos@midpointmarkets.org>
2023-12-21 19:22:29 -0800

    moving new CSS to web-app dir and removing working dir

--- web-app/js/nav-bar.js
+++
@@ -1,9 +0,0 @@
-const currentUrl = window.location.href;
-
-const navLinks = document.querySelectorAll('nav ul li a');
-
-navLinks.forEach(link => {
-    if (link.href === currentUrl) {
-        link.classList.add('active');
-    }
-});
