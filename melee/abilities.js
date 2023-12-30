76ecd7c93d5bbab3534379dafb62f6a4e7fdfb7f
akritia <akritiapvme@gmail.com>
2023-12-30 21:06:18 +0100

    melee setup

---
+++ melee/abilities.js
@@ -0,0 +1,12 @@
+const slice = require("./abils/slice");
+
+const abilities = {
+  'Slice': {
+    title: 'Slice',
+    calc: slice,
+    icon: 'https://imgur.com/MVogErK.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+}
+
+export {abilities}
