a727cbca632a61bb9bf2a991c82a93bc75971318
akritia <akritiapvme@gmail.com>
2024-03-02 22:47:34 +0100

    missing letter

--- js/ranged/ranged_on_npc.js
+++ js/ranged/ranged_on_npc.js
@@ -56,7 +56,7 @@ class OnNPC {
     }
 
     calcCustom(dmg,settings) {
-      return Math.floor(dmg * (1 + setting['custom on-npc']/10));
+      return Math.floor(dmg * (1 + settings['custom on-npc']/10));
     }
 
     calcRedbeam(dmg,flag) {
