6a2854057b36ce807a3373a01305d88998392089
akritia <akritiapvme@gmail.com>
2024-04-22 20:59:42 +0200

    apply swift to dirty bolg

--- js/ranged/abils/bolg_proc_dirty.js
+++ js/ranged/abils/bolg_proc_dirty.js
@@ -1,7 +1,11 @@
 import bolg_base_dirty from './bolg_base_dirty.js';
 
 function bolg_proc_dirty(type, settings, numberOfHits) {
-    const base = bolg_base_dirty(settings['bolg damage dirty'], type, settings, numberOfHits);
+    let proccing_damage = settings['bolg damage dirty']
+    if (settings['swift'] === true) {
+        proccing_damage = Math.floor(1.5 * proccing_damage)
+    }
+    const base = bolg_base_dirty(proccing_damage, type, settings, numberOfHits);
     return  [base[base.length-1]];
 }
 
