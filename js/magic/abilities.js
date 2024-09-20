a327f537416bb9f56f320b2b691622916c74c805
ocditia <akritiapvme@gmail.com>
2024-09-20 13:22:51 +0200

    probability fix

--- js/magic/abilities.js
+++ js/magic/abilities.js
@@ -8,5 +8,108 @@ const abilities = {
     icon: 'assets/ability_icons/magic/30x30/wrack.png',
     weapons: ['2h','Dw','Md','Ms'],
   },
+  'Wrackdfs5': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+  'Wrackj54': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+  'Wracksr6': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+  'Wrack56u': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+  'Wrackfgj': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+  'Wracke53': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+  'Wrack46dr': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+  'Wrack6342': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+  'Wrack34': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+  'Wracksdfher': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+  'Wrackgsfg': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+  'Wrackewdfs': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+  'Wrackywet': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+  'Wrackwrt': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+  'Wrackgdf': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+  'Wrack2': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+  'Wrackasd': {
+    title: 'Wrack',
+    calc: wrack,
+    icon: 'assets/ability_icons/magic/30x30/wrack.png',
+    weapons: ['2h','Dw','Md','Ms'],
+  },
+}  
 
 export {abilities}
