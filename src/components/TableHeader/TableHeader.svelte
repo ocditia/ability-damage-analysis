b52c2594faaa5414a72233dbd4f4a7692b0ead5b
Gamedolf <gamedolf@live.co.uk>
2024-11-16 21:10:38 -0800

    Create TableHeader.svelte

---
+++ src/components/TableHeader/TableHeader.svelte
@@ -0,0 +1,12 @@
+<script>
+    let { onclick, sortType, title } = $props()
+</script>
+
+<button {onclick}>
+    {title}
+    {#if sortType === 'asc'}
+        🔼
+    {:else if sortType === 'desc'}
+        🔽
+    {/if}
+</button>
