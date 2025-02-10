2373e11483a8e8b7234afa134e2db70ff7f0aa9b
ocditia <96238651+ocditia@users.noreply.github.com>
2025-02-10 21:01:34 +0100

    typo

--- src/lib/calc/damage_calc.js
+++ src/lib/calc/damage_calc.js
@@ -1036,7 +1036,7 @@ function calc_on_npc(settings, dmgObject) {
         ) {
             dmgObject['damage list'][i] = Math.floor(dmgObject['damage list'][i] * 1.2);
         } else if (
-            settings[SETTINGS.ENDURING_RUIN_BLEED] === ENDURING_RUIN_HIT_VALUES.ENCHANTED&&
+            settings[SETTINGS.ENDURING_RUIN_BLEED] === ENDURING_RUIN_HIT_VALUES.ENCHANTED &&
             abils[settings['ability']]['ability classification'] === 'bleed'
         ) {
             dmgObject['damage list'][i] = Math.floor(dmgObject['damage list'][i] * 1.25);
