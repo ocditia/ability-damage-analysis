fb7b655f9e237006f233ea6235517a335fc66085
ocditia <96238651+ocditia@users.noreply.github.com>
2025-03-04 00:11:44 +0100

    Update damage_calc.js

--- src/lib/calc/damage_calc.js
+++ src/lib/calc/damage_calc.js
@@ -473,7 +473,7 @@ function set_min_var(settings, dmgObject) {
         // greater barge tick bonus
         if (settings['ability'] === ABILITIES.GREATER_BARGE) {
             min_percent = min_percent + Math.min(0.05 * settings[SETTINGS.TIME_SINCE_ATTACK], 0.5);
-            var_percent = var_percent + Math.min(0.02 * settings[SETTINGS.TIME_SINCE_ATTACK], 0.7);
+            var_percent = var_percent + Math.min(0.02 * settings[SETTINGS.TIME_SINCE_ATTACK], 0.2);
         }
 
         // icy tempest
