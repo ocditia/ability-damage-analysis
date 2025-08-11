2c56a7ab26e7ba81f66756522cf9b8ed95ec75e6
ocditia <96238651+ocditia@users.noreply.github.com>
2025-08-11 17:01:36 +0000

    typo

--- src/lib/calc/const.js
+++ src/lib/calc/const.js
@@ -5292,8 +5292,8 @@ const abils = {
         'on-hit effects': true, // does the ability get on-hit effects
         'crit effects': true, // can the ability crit
         'damage potential effects': true, // is the ability affected by damage potential
-        'ability classification': 'combatv2_passive_ability', // bleed, dot, burn, channel, regular, multihit
-        'ability type': 'basic', // basic, threshold, special attack, ability (necromancy classification), ultimate
+        'ability classification': 'regular', // bleed, dot, burn, channel, regular, multihit
+        'ability type': 'combatv2_passive_ability', // basic, threshold, special attack, ability (necromancy classification), ultimate
         'main style': 'magic',
         'damage type': 'magic' // basic, threshold, special attack, ability (necromancy classification), ultimate
     },
