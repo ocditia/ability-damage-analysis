37e91ab1204b59b7d854d2391743dbbab0aa36b6
ocditia <96238651+ocditia@users.noreply.github.com>
2026-02-17 19:41:25 +0000

    magma

--- src/lib/calc/const.js
+++ src/lib/calc/const.js
@@ -4796,7 +4796,7 @@ const abils = {
     [ABILITIES.MAGMA_BETA]: {
         // ability name
         'min hit': 0.35, // min % of abil expressed as a decimal
-        'var hit': 0.05,
+        'var hit': 0.1,
         'on-hit effects': true, // does the ability get on-hit effects
         'crit effects': false, // can the ability crit
         'damage potential effects': true, // is the ability affected by damage potential
