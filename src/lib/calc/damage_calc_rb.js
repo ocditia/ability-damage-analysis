db9447e87e142fc715231a013680a5b9298ec5f9
ocditia <96238651+ocditia@users.noreply.github.com>
2026-03-30 17:40:22 +0000

    keris

--- src/lib/calc/damage_calc_rb.js
+++ src/lib/calc/damage_calc_rb.js
@@ -53,7 +53,6 @@ function calc_base_ad(settings) {
     let buff = 1 + settings[SETTINGS.ERUPTIVE] * 0.005;
     if (settings[SETTINGS.EQ_PERK] > 0) {
         buff += 0.06 + settings[SETTINGS.EQ_PERK] * 0.02;
-        console.log(buff)
     }
     const rawAD = base_AD;
     base_AD = Math.floor(base_AD * buff);
@@ -202,7 +201,7 @@ function calc_boosted_ad(settings, dmgObject) {
                 base_ad_boost += 0.333;
             }
             else if ([SETTINGS.MELEE_MH_VALUES.KERIS_PROC, SETTINGS.MELEE_MH_VALUES.PRIMED_KERIS_PROC, SETTINGS.MELEE_MH_VALUES.CONSECRATED_KERIS_PROC].includes(settings[SETTINGS.MH])) {
-                base_ad_boost += 1;
+                base_ad_boost += 2;
             }
         }
 
