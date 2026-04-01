e198fff7168d09be6c41c5edd8ac69f258a8e67b
Marco648135 <Marco648135@users.noreply.github.com>
2026-04-01 23:27:14 +0100

    Iban spec to common

--- src/lib/data/abilities.ts
+++ src/lib/data/abilities.ts
@@ -2265,7 +2265,7 @@ export const abils: Record<ABILITIES, AbilityInfo> = {
             1: [ABILITIES.WILD_MAGIC_HIT, 'next hit', ABILITIES.WILD_MAGIC_HIT]
         },
         cooldown: 5.4,
-        hitTimings: [2, 2],
+        hitTimings: [1, 1],
         title: 'Wild magic',
         icon: '/ability_icons/magic/30x30/wild_magic.png',
     },
@@ -2797,8 +2797,7 @@ export const abils: Record<ABILITIES, AbilityInfo> = {
         cooldown: 0,
         hitTimings: [0, 1, 2, 3, 4],
         duration: 5,
-        adrenaline: 50
-    ,
+        adrenaline: 50,
         title: 'Tempest of Armadyl',
         icon: '/ability_icons/magic/30x30/armadyl_battlestaff-bg.png',
     },
@@ -2813,12 +2812,11 @@ export const abils: Record<ABILITIES, AbilityInfo> = {
         abilityType: 'special attack', // basic, threshold, special attack, ability (necromancy classification), ultimate
         mainStyle: 'magic',
         damageType: 'magic',
-        adrenaline: 50
-    ,
+        adrenaline: 50,
         cooldown: 0,
         title: 'Iban blast',
         icon: '/ability_icons/magic/30x30/iban_staff.png',
-        common: false
+        common: true
     },
     [ABILITIES.CLAWS_OF_GUTHIX]: {
         // ability name
